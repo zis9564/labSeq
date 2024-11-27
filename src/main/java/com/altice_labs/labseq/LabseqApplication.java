be522cbd4eda812bfad3db6547a8db2194f5e314
ivan.zakharov <ivan.zaharov@irlix.ru>
2024-11-27 20:14:33 +0000

    Initial commit

---
+++ src/main/java/com/altice_labs/labseq/LabseqApplication.java
@@ -0,0 +1,13 @@
+package com.altice_labs.labseq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabseqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabseqApplication.class, args);
+	}
+
+}
